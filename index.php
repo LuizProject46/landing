3c5cedbb8132b50b4ffcf8c86d4cb1e4f72fdfc8
LuizGustavoMoreira <luiz@vpeventos.com>
2020-08-11 00:01:46 -0300

    commit

---
+++ index.php
@@ -0,0 +1,193 @@
+
+<html>
+<title>lgmprojects - LGM</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+    .btn-send{transition: 1s;
+    width: 100%;
+    height: 40px;
+    border: none;
+    border-radius: 4px;
+    background-color: #333;
+    color: white;
+    font-size: 16px;
+    }
+
+  .btn-send:hover{
+    transition: 1s;
+    background: #f5f5f5;
+    color: #333;
+    box-shadow: 1px 1px 7px #333;
+    font-size: 16px;
+  }
+</style>
+<body>
+
+<!-- Navbar (sit on top) -->
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-wide w3-padding w3-card">
+    <a href="#home" class="w3-bar-item w3-button"><b>LGM</b> Projects</a>
+    <!-- Float links to the right. Hide them on small screens -->
+    <div class="w3-right w3-hide-small">
+      <a href="#portifolio" class="w3-bar-item w3-button">Portifólio</a>
+      <a href="#sobre" class="w3-bar-item w3-button">Sobre</a>
+      <a href="#contato" class="w3-bar-item w3-button">Contato</a>
+    </div>
+  </div>
+</div>
+
+<!-- Header -->
+
+<header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
+  <img class="w3-image" src="img/main.jpg" alt="Architecture" width="1500" height="800">
+  
+  <div class="w3-display-middle w3-margin-top w3-center">
+    <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min"><b>LGM</b></span> <span class="w3-hide-small w3-text-light-grey">Projects</span></h1>
+  </div>
+</header>
+
+<!-- Page content -->
+<div class="w3-content w3-padding" style="max-width:1564px">
+
+  <!-- Project Section -->
+  <div class="w3-container w3-padding-32" id="portifolio">
+    <h2 style="text-align: center;
+    font-size: xxx-large;
+    margin: 0px;
+    color: #333;
+    font-family: monospace;
+    font-weight: 600;" class=" w3-border-light-grey w3-padding-16">Portifólio</h2>
+  </div>
+
+  <div class="w3-container w3-padding-32" id="portifolio">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">SocialSociety - Rede Social</h3>
+  </div>
+  <div class="w3-row-padding">
+    <div style="width:50%;" class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">SocialSociety - Login</div>
+        <img src="img/sc1.png" alt="SocialSociety - 1" style="width:100%">
+      </div>
+    </div>
+    <div style="width:50%;" class="w3-col l3 m6 w3-margin-bottom">
+      <div  class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">SocialSociety - Timeline</div>
+        <img src="img/sc2.png" alt="SocialSociety - 2" style="width:100%">
+      </div>
+     <!--- 
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Renovated</div>
+        <img src="/w3images/house3.jpg" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Barn House</div>
+        <img src="/w3images/house4.jpg" alt="House" style="width:100%">
+      </div>
+    </div> -->
+  </div>
+
+  <!-- <div class="w3-row-padding">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Summer House</div>
+        <img src="/w3images/house2.jpg" alt="House" style="width:99%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Brick House</div>
+        <img src="/w3images/house5.jpg" alt="House" style="width:99%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Renovated</div>
+        <img src="/w3images/house4.jpg" alt="House" style="width:99%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Barn House</div>
+        <img src="/w3images/house3.jpg" alt="House" style="width:99%">
+      </div>
+    </div>
+  </div> -->
+
+  <!-- About Section -->
+  <div class="w3-container w3-padding-32" id="sobre">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Sobre</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
+  </div>
+
+  <!-- <div class="w3-row-padding w3-grayscale">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="/w3images/team2.jpg" alt="John" style="width:100%">
+      <h3>John Doe</h3>
+      <p class="w3-opacity">CEO & Founder</p>
+      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+      <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="/w3images/team1.jpg" alt="Jane" style="width:100%">
+      <h3>Jane Doe</h3>
+      <p class="w3-opacity">Architect</p>
+      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+      <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="/w3images/team3.jpg" alt="Mike" style="width:100%">
+      <h3>Mike Ross</h3>
+      <p class="w3-opacity">Architect</p>
+      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+      <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="/w3images/team4.jpg" alt="Dan" style="width:100%">
+      <h3>Dan Star</h3>
+      <p class="w3-opacity">Architect</p>
+      <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+      <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+    </div>
+  </div> -->
+
+  <!-- Contact Section -->
+  <div class="w3-container w3-padding-32" id="contato">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Contato</h3>
+    <p>Entre em contato para freela</p>
+    <form action="sendMail.php" method="POST" target="_blank">
+      <input class="w3-input w3-border" type="text" placeholder="Nome" required name="Name">
+      <input class="w3-input w3-section w3-border" type="text" placeholder="Email" required name="Email">
+      <input class="w3-input w3-section w3-border" type="text" placeholder="Assunto" required name="Subject">
+      <input class="w3-input w3-section w3-border" type="text" placeholder="Mensagem" required name="Comment">
+      <button class="btn-send" type="submit">
+        <i class="fa fa-paper-plane"></i> Enviar
+      </button>
+    </form>
+  </div>
+  
+<!-- Image of location/map -->
+<!-- <div class="w3-container">
+  <img src="img/img1.jpg" class="w3-image" style="width:100%">
+</div> -->
+
+<!-- End page content -->
+</div>
+
+
+<!-- Footer -->
+<footer  style="background:black;color:white;"class="w3-center  w3-padding-16">
+  <p>Powered by LGM <?=date("yy")?></p>
+</footer>
+
+</body>
+</html>
+
